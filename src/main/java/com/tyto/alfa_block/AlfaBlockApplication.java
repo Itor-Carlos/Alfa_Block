092340d873edc0422162e3f5946ab00d17a69199
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2023-04-25 21:43:48 -0300

    first commit

---
+++ src/main/java/com/tyto/alfa_block/AlfaBlockApplication.java
@@ -0,0 +1,13 @@
+package com.tyto.alfa_block;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfaBlockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyto/alfa_block/AlfaBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyto.alfa_block;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaBlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
